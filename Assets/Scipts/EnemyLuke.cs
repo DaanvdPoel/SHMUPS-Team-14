6b72c5730ae6add811896d5b984541694bcd2fc8
LukeR2003 <78425989+LukeR2003@users.noreply.github.com>
2021-04-08 17:16:17 +0200

    Enemy en Enemyspawner
    ik heb hier een basis van een enemy en een enemy spawner

---
+++ Assets/Scipts/EnemyLuke.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyLuke : MonoBehaviour
+{
+    private int health;
+    private float randomCooldown;
+    private bool canMove;
+    private float distance = 1;
+    private float speed = 10;
+
+    private GameObject target;
+    //private Playerclass player;
+
+
+    void Start()
+    {
+        canMove = true;
+        target = GameObject.FindGameObjectWithTag("Target");
+        //player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    void Update()
+    {
+        if (canMove == true)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, target.transform.position, distance * (speed * Time.deltaTime));
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Bullet"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scipts/EnemySpawnerLuke.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawnerLuke : MonoBehaviour
+{
+    [SerializeField] private float spawnCooldown = 1;
+    [SerializeField] private int spawnCount = 2;
+    [SerializeField] private GameObject enemyType;
+
+    private float spawnTimer;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (spawnTimer > 0)
+        {
+            spawnTimer = spawnTimer - Time.deltaTime;
+        }
+
+        if (spawnCount > 0)
+        {
+            if (spawnTimer <= 0)
+            {
+                Instantiate(enemyType);
+                spawnTimer = spawnCooldown;
+                spawnCount--;
+            }
+        }
+    }
+}
