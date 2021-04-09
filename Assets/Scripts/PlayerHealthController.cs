a094a25bcc4dd6737d60ab6d2fb087d71f5f39a0
Reliii <rensvanderlit@gmail.com>
2021-04-09 12:51:12 +0200

    Player Health Controller

---
+++ Assets/Scripts/PlayerHealthController.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+
+s
+public class PlayerHealthController : MonoBehaviour
+{
+    #region Properties
+
+    
+    [SerializeField] private int healthInitial = 3;
+
+    
+    private int healthCurrent;
+
+    #endregion
+
+    #region Initialisation methods
+
+    
+    void Start()
+    {
+        
+        ResetHealth();
+    }
+  public void ResetHealth()
+    {
+      
+        healthCurrent = healthInitial;
+    }
+
+    #endregion
+
+    #region Gameplay methods
+
+    
+    public void TakeDamage(int damageAmount)
+    {
+       
+        healthCurrent -= damageAmount;
+
+       
+        if (healthCurrent <= 0)
+        {
+            
+            Destroy(gameObject);
+
+            
+        }
+    }
+
+  
+    public void Heal(int healAmount)
+    {
+       
+        healthCurrent += healAmount;
+
+       
+        if (healthCurrent > healthInitial)
+        {
+            
+            ResetHealth();
+        }
+    }
+
+    #endregion
+}
